4d949915bd7326649a9defd18ededa9a2adca8d9
Om Saudager <84255810+om820@users.noreply.github.com>
2025-01-03 02:11:10 +0530

    Theme related changes
    1. Added two icon for dark theme and light theme
    2. Used them as images for button
    3. Changed their sizes using CSS
    4. Toggle the icons based on what theme the page is on using different ID's for each icon images
    5. Used basic JS functions like inline and none to display them using the class of the theme.

--- ThemeToggle.js
+++ ThemeToggle.js
@@ -2,4 +2,14 @@ const themeToggle = document.getElementById("themeToggle");
 
 themeToggle.addEventListener('click', function(){
     document.body.classList.toggle('lightTheme');
+
+    if(document.body.classList.contains('lightTheme')){
+        lightIcon.style.display = 'inline'
+        darkIcon.style.display = 'none'
+    }
+    else{
+        lightIcon.style.display = 'none'
+        darkIcon.style.display = 'inline'
+        }
 });
+
